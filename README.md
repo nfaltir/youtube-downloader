adcd5a3395aa1350be3b2a1e41bc10e2eab436bc
nfaltir <nfaltir@protonmail.com>
2023-01-09 13:34:28 -0800

    added readme.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# Youtube Video Downloader
+
+<br>
+
+## App Overview
+
+streamlit web app that takes a youtube link and downloads a video or audio file based on user preference.
+
+<br>
+
+## Packages
+
+```python
+
+pandas==1.5.2
+pytube==12.1.2
+streamlit==1.16.0
+
+```
+
+<br><i>App Demo</i>
+<img src="a.png"
+     alt="app demo"
+     style="float: left; margin-right: 10px; margin-bottom: 50px;" />
+
+
+<br>
+
+VISIT 👉 <a href="https://tinyurl.com/2jquu7br " target="_blank">Demo Site</a>
--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from pytube import YouTube
 import base64
 from io import BytesIO
 
+st.set_page_config(page_title="Youtube Downloader", page_icon="⭕️")
 st.markdown("<h1 style='text-align: center; color: #FF0000; margin-bottom:50px; font-size: 4.5rem;'>Youtube <span style='text-align: center; color: #555;'>Downloader</span></h1>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
@@ -14,8 +15,8 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', uns
 def main():
     link = st.text_input("Enter or paste youtube video link:")
     option = st.radio('select type of download', (
-        'audio',
-        'video'
+        'video',
+        'audio'
     ))
 
     matches = ["audio", "video"]
